343131869a7f55b1780c7d1a9c597d2fe74e23dd
marizal-akib <marizalakib@gmail.com>
2023-11-20 17:33:37 +0000

    set users get api

--- index.js
+++ index.js
@@ -25,10 +25,27 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         
-        const userCollection = client.db('bistroDB').collection('user')
+        const userCollection = client.db('bistroDB').collection('users')
         const menuCollection = client.db('bistroDB').collection('menu')
         const reviewCollection = client.db('bistroDB').collection('reviewes')
         const cartCollection = client.db('bistroDB').collection('carts')
+
+        // users related api
+        app.get( "/users", async(req,res) =>{
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+        app.post('/users', async(req,res) =>{
+            const user = req.body;
+            console.log(user);
+            const query = {email:user.email}
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({message: 'user already exists', insertedId: null})
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
 
         app.get('/menu', async(req,res) =>{
             const result = await menuCollection.find().toArray();
