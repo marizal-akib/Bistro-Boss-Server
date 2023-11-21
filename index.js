8453df7a551b4b3d56197f494c633a1dc2eca0b1
marizal-akib <marizalakib@gmail.com>
2023-11-21 11:23:09 +0000

    set middlewear for verifyToke

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 
 //middleware
 app.use(cors());
@@ -24,33 +25,84 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-        
+
         const userCollection = client.db('bistroDB').collection('users')
         const menuCollection = client.db('bistroDB').collection('menu')
         const reviewCollection = client.db('bistroDB').collection('reviewes')
         const cartCollection = client.db('bistroDB').collection('carts')
 
+        // jwt related API
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({ token });
+
+        })
+
+        // middlewares
+        const verifyToken = (req, res, next)=>{
+            console.log("in verify token",req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({message: 'unauthorized access'});
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, decoded) =>{
+                if(err){
+                    return res.status(401).send({message: 'unauthorized access'})
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
         // users related api
-        app.get( "/users", async(req,res) =>{
+        app.get("/users",verifyToken, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         })
-        app.post('/users', async(req,res) =>{
+        const verifyAdmin = async(req,res, next) =>{
+            const email = req.decoded.email;
+            const query = {email:email};
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({message:'forbidden access'})
+                
+            }
+            next()
+        }
+        app.get('/users/admin/:email', verifyToken, verifyAdmin, async(req,res) =>{
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({message:'forbidden access'})
+                
+            }
+            const query ={email:email};
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if(user){
+                admin = user?.role === 'admin';
+            }
+            res.send({admin});
+        })
+        app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
-            const query = {email:user.email}
+            const query = { email: user.email }
             const existingUser = await userCollection.findOne(query);
             if (existingUser) {
-                return res.send({message: 'user already exists', insertedId: null})
+                return res.send({ message: 'user already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
-        
-        app.patch('users/admin/:id', async(req,res)=>{
+
+        app.patch('/users/admin/:id',verifyAdmin,verifyToken, async (req, res) => {
             const id = req.params.id;
-            const query ={_id: new ObjectId(id)};
-            const updatedDoc ={
+            const query = { _id: new ObjectId(id) };
+            const updatedDoc = {
                 $set: {
                     role: "admin"
                 }
@@ -59,20 +111,20 @@ async function run() {
             res.send(result);
 
         })
-        app.delete('/users/:id', async(req,res)=>{
+        app.delete('/users/:id', verifyAdmin, verifyToken, async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query);
             res.send(result);
 
         })
 
         // menu
-        app.get('/menu', async(req,res) =>{
+        app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
         })
-        app.get('/reviews', async(req,res) =>{
+        app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray();
             res.send(result);
         })
@@ -80,24 +132,24 @@ async function run() {
 
 
         // cart Collection
-        app.get('/carts', async(req,res) =>{
-            const result = await  cartCollection.find().toArray();
+        app.get('/carts', async (req, res) => {
+            const result = await cartCollection.find().toArray();
             res.send(result);
         })
-        app.post('/carts', async(req,res) =>{
+        app.post('/carts', async (req, res) => {
             const item = req.body;
             console.log(item);
             const result = await cartCollection.insertOne(item);
             res.send(result);
         })
 
-        app.delete('/carts/:id', async(req,res) =>{
+        app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: new ObjectId(id) }
+            const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
-        
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
